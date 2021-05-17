e6447943c35848468d8ebbc91dbbf6eaed0fbd4e
Hiral-Jani <61633839+Hiral-Jani@users.noreply.github.com>
2021-05-17 17:06:46 -0400

    Update index.php

--- 04-league-table/index.php
+++ 04-league-table/index.php
@@ -2,28 +2,44 @@
 
 class LeagueTable
 {
-	public function __construct(array $players)
-	{
-		$this->standings = [];
-		foreach ($players as $index => $p) {
-			$this->standings[$p] = [
-				'index'        => $index,
-				'games_played' => 0,
-				'score'        => 0
-			];
-		}
-	}
+    public function __construct(array $players)
+    {
+        $this->standings = [];
+        foreach ($players as $index => $p) {
+            $this->standings[$p] = [
+                'index'        => $index,
+                'games_played' => 0,
+                'score'        => 0,
+                'player'       => $p
+            ];
+        }
+    }
 
-	public function recordResult($player, $score)
-	{
-		$this->standings[$player]['games_played']++;
-		$this->standings[$player]['score'] += $score;
-	}
+    public function recordResult($player, $score)
+    {
+        $this->standings[$player]['games_played']++;
+        $this->standings[$player]['score'] += $score;
+    }
+    
+    public function playerRank($rank) 
+    {
+        $standings = $this->standings;
+        usort($standings, function ($standing1, $standing2) {
+            if ($standing1['score'] === $standing2['score']) {
+                if ($standing1['games_played'] === $standing2['games_played']) {
+                    $sort = $standing1['index'] < $standing2['index'] ? -1 : 1;
+                } else {
+                    $sort = $standing1['games_played'] < $standing2['games_played'] ? -1 : 1;
+                }
+            } else {
+                $sort = $standing1['score'] > $standing2['score'] ? -1 : 1;
+            }
 
-	public function playerRank($rank)
-	{
-		return '';
-	}
+            return $sort;
+        });
+
+        return $standings[$rank-1]['player'];
+    }
 }
 
 $table = new LeagueTable(array('Mike', 'Chris', 'Arnold'));
